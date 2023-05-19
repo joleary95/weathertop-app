c2e75e3a7dad353fb757d22611c59699d4b4cea5
Jonathan O'Leary <jonathanoleary24@gmail.com>
2023-05-19 02:37:22 +0100

    members functions added - signup/login. customize

--- app/controllers/Dashboard.java
+++ app/controllers/Dashboard.java
@@ -2,6 +2,7 @@ package controllers;
 
 import java.util.List;
 
+import models.Member;
 import models.Station;
 import play.Logger;
 import play.mvc.Controller;
@@ -12,24 +13,29 @@ public class Dashboard extends Controller
   public static void index()
   {
     Logger.info("Rendering Admin");
-
-    List<Station> stations = Station.findAll();
+    Member member = Accounts.getLoggedInMember();
+    List<Station> stations = member.stations;
     render ("dashboard.html", stations);
   }
 
   public static void deleteStation(Long id)
   {
+    Member member = Accounts.getLoggedInMember();
     Station station = Station.findById(id);
     Logger.info ("Removing" + station.name);
+    member.stations.remove(station);
+    member.save();
     station.delete();
     redirect ("/dashboard");
   }
 
   public static void addStation(String name)
   {
+    Member member = Accounts.getLoggedInMember();
     Station station = new Station(name);
+    member.stations.add(station);
     Logger.info ("Adding a new station called " + name);
-    station.save();
+    member.save();
     redirect ("/dashboard");
   }
 }
