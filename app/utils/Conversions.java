7eb34375f04ec3286acbc31fd056ad685741dfd4
Jonathan O'Leary <jonathanoleary24@gmail.com>
2023-05-21 16:07:33 +0100

    Final Release 3 (part) - only implemented release 3 feature is delete readings button

--- app/utils/Conversions.java
+++ app/utils/Conversions.java
@@ -25,9 +25,7 @@ public class Conversions extends Utils{
         }
 
     public static String codeToWeatherDescription(int code) {              // Converting code to Description
-
         // code is in numerical order increasing by 100. dividing by 100 to make the code more readable.
-
         switch (code/100) {
             case 1:
                 return "Clear";
@@ -48,28 +46,47 @@ public class Conversions extends Utils{
         }
     }
 
-    /*Using the Modulo to convert the wind direction to an integer by splitting the
-      circle into 16 even degrees of 22.5 and then
-      dividing by that number to find the corresponding index.
-    */
     public static String getCompassDirection(float windDirection) {
 
-        String[] compassDirection = {"N", "NNE", "NE", "ENE", "E", "ESE",
-                "SE", "SSE", "S", "SSW", "SW", "WSW", "W", "WNW", "NW", "NNW"};
-
-        int mainNumber = (int) windDirection; // get the integer part of the wind speed value
-        float decimalPart = windDirection - mainNumber; // get the decimal part of the wind speed value
-        int CompassDirection;
-
-        if (decimalPart > 0.25 && decimalPart < 0.75) {
-            CompassDirection = (int) Math.floor(((windDirection % 360) + 11.25) / 22.5);
-        } else {
-            CompassDirection = (int) Math.ceil(((windDirection % 360) + 11.25) / 22.5);
+                if ((windDirection >= 348.75) && (windDirection <= 360)) {
+                    return "North";
+                } else if ((windDirection >= 0) && (windDirection <= 11.25)) {
+                    return "North";
+                } else if ((windDirection >= 11.25) && (windDirection <= 33.75)) {
+                    return "North NE";
+                } else if ((windDirection >= 33.75) && (windDirection <= 56.25)) {
+                    return "North East";
+                } else if ((windDirection >= 56.25) && (windDirection <= 78.75)) {
+                    return "East NE";
+                } else if ((windDirection >= 78.75) && (windDirection <= 101.25)) {
+                   return "East";
+                } else if ((windDirection >= 101.25) && (windDirection <= 123.75)) {
+                    return "East SE";
+                } else if ((windDirection >= 123.75) && (windDirection <= 146.25)) {
+                    return "South East";
+                } else if ((windDirection >= 146.25) && (windDirection <= 168.75)) {
+                    return "South SE";
+                } else if ((windDirection >= 168.75) && (windDirection <= 191.25)) {
+                    return "South";
+                } else if ((windDirection >= 191.25) && (windDirection <= 213.75)) {
+                    return "South SW";
+                } else if ((windDirection >= 213.75) && (windDirection <= 236.25)) {
+                    return "South West";
+                } else if ((windDirection >= 236.25) && (windDirection <= 258.75)) {
+                    return  "West SW";
+                } else if ((windDirection >= 258.75) && (windDirection <= 281.25)) {
+                    return "West";
+                } else if ((windDirection >= 281.25) && (windDirection <= 303.75)) {
+                    return "West NW";
+                } else if ((windDirection >= 303.75) && (windDirection <= 326.25)) {
+                    return "North West";
+                } else if ((windDirection >= 326.25) && (windDirection <= 348.75)) {
+                    return "North NW";
+                } else if ((windDirection < 0) || (windDirection > 360)) {
+                    return "Enter wind direction";
+                }
+            return "Enter wind direction";
         }
-        CompassDirection = CompassDirection % 16;
-        return compassDirection[CompassDirection];
-
-    }
 
     public static double getWindChill(float temp, double v) {
 
